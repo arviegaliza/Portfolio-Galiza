4e7b10429346aede5a5f51cd8c2962e95efa5814
rvqwry_ <arviegaliza4@gmail.com>
2026-07-17 23:29:33 +0800

    Update mailer.js

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,7 +1,11 @@
 const nodemailer = require("nodemailer");
 
 function createTransporter() {
-  return nodemailer.createTransport({
+  console.log("===== BREVO SMTP CONFIG =====");
+  console.log("BREVO_USER:", process.env.BREVO_USER);
+  console.log("BREVO_PASS exists:", !!process.env.BREVO_PASS);
+
+  const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
@@ -9,7 +13,14 @@ function createTransporter() {
       user: process.env.BREVO_USER,
       pass: process.env.BREVO_PASS,
     },
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
+    logger: true,
+    debug: true,
   });
+
+  return transporter;
 }
 
 module.exports = createTransporter;
